6a9158db3cb0707d35025d6bed83e83e8138ebe4
Claudia <hello@allegro-online.net>
2018-08-04 07:45:03 +0200

    Refactor tabs

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,30 +5,29 @@ import React, { Component } from 'react';
 
 class Error extends Component {
 
-	render = () => {
+    render = () => {
 
-		return (
-			
-			<div>
-				<nav className="navbar">
-	            	<div className="navbar-header">
-	                	<h1 tabIndex="0" className="site-name">Discover Japan</h1>
-	               	</div>
-	            </nav>
-				<div className="main-container">
-	            	<main className="map-container" role="main">
-	                	<div className="api-failure-container">
-							<div className="api-failure">
-								<p className="alert-text">Sorry, the data could not be loaded.</p>
-								<p className="alert-text">See the JavaScript console for technical details.</p>
-							</div>
-						</div>
-					</main>
-				</div>
-			</div>	
+        return (
 
-		);
-	}	
+            <div>
+                <nav className="navbar">
+                    <div className="navbar-header">
+                        <h1 tabIndex="0" className="site-name">Discover Japan</h1>
+                    </div>
+                </nav>
+                <div className="main-container">
+                    <main className="map-container" role="main">
+                        <div className="api-failure-container">
+                            <div className="api-failure">
+                                <p className="alert-text">Sorry, the data could not be loaded.</p>
+                                <p className="alert-text">See the JavaScript console for technical details.</p>
+                            </div>
+                        </div>
+                    </main>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Error;
