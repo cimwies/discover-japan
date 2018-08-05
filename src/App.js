1878424ac5721e93bb8e90ec6d2d6f5635a39212
Claudia <hello@allegro-online.net>
2018-08-05 16:17:35 +0200

    Fix lost div

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ class App extends Component {
 
 
         return (
+            <div className="App">
                 <Filter handleQuery = {this.handleQuery} />
                 <MapContainer 
                     google = {this.props.google}
